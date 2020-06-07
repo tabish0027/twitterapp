264b6a4e492ed231e14c1dac7125bb179c410d8f
tabishmanzoor <tallal.javed@outlook.com>
2020-06-07 19:01:16 +0500

    modification

--- twitterapi/index.php
+++ twitterapi/index.php
@@ -13,14 +13,14 @@ include './header.php';
 ?>
 <div class="container">
   <div class="margin10"></div>
-  
+  <!--
   <?php if ($_SESSION["e_msg"] <> "") { ?>
     <div class="alert alert-dismissable alert-danger">
       <button data-dismiss="alert" class="close" type="button">x</button>
       <p><?php echo $_SESSION["e_msg"]; ?></p>
     </div>
   <?php } ?>
-  
+  -->
   <div class="col-sm-3 col-sm-offset-4">
     <a class="btn btn-block btn-social btn-twitter" href="twitter_login.php">
             <i class="fa fa-twitter"></i> Login with twitter
@@ -28,7 +28,7 @@ include './header.php';
   </div>
 </div>
 <?php
-include './footer.php';
+// include './footer.php';
 // unset if after it display the error.
 $_SESSION["e_msg"] = "";
 ?>
