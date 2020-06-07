4eed2a5831f56f4f5bfa7fc901c7c63d1a311ccd
tabish0027 <m.tabish.manzoor@gmail.com>
2020-06-07 15:52:07 +0500

    Update callback.php

--- callback.php
+++ callback.php
@@ -2,3 +2,17 @@
 
 echo "helloworld";
 ?>
+
+
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+<input type="button">
+
+</body>
+</html>
