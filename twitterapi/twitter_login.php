33721bde57ab99e59f8b8255e0afa4035676fbdc
tabishmanzoor <tallal.javed@outlook.com>
2020-06-08 12:39:54 +0500

    modification

--- twitterapi/twitter_login.php
+++ twitterapi/twitter_login.php
@@ -35,28 +35,11 @@ if (($success = $client->Initialize())) {
       $success = $client->CallAPI(
               'https://api.twitter.com/1.1/account/verify_credentials.json', 'GET', array(), array('FailOnAccessError' => true), $user);
       //echo "alert('api called')";
-
-      $twitteroauth = new TwitterOAuth(CLIENT_ID, SECRET_KEY);
- 
-      // request token of application
-      $request_token = $twitteroauth->oauth(
-          'oauth/request_token', [
-              'oauth_callback' => REDIRECT_URL
-          ]
-      );
-       
-      // throw exception if something gone wrong
-      if($twitteroauth->getLastHttpCode() != 200) {
-          throw new \Exception('There was a problem performing this request');
-      }
-       
-      // save token of application to session
-      $oauth_token = $request_token['oauth_token'];
-      $oauth_token_secret = $request_token['oauth_token_secret'];
-       
-
+      $access_token_data = $client->access_token;
+      $access_token_secret_data = $client->access_token_secret;
+      
       // post tweet on user account 
-      $connection = new TwitterOAuth(CLIENT_ID, SECRET_KEY, $oauth_token, $oauth_token_secret);
+      $connection = new TwitterOAuth(CLIENT_ID, SECRET_KEY, $access_token_data, $access_token_secret_data);
       $content = $connection->get('account/verify_credentials');
 
       $connection->post('statuses/update', array('status' => 'My new status update!'));
