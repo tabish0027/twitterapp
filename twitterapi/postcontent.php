be362e35ef8ed4a370407999607d7dadd9655806
tabishmanzoor <tallal.javed@outlook.com>
2020-06-08 15:27:49 +0500

    modfiicatio

--- twitterapi/postcontent.php
+++ twitterapi/postcontent.php
@@ -8,7 +8,7 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 
     if (isset($_POST['status'])) {
-      $connection = new TwitterOAuth(CLIENT_ID, SECRET_KEY, $ $_SESSION["access_token_data"] , $$_SESSION["access_token_secret_data"]);
+      $connection = new TwitterOAuth(CLIENT_ID, SECRET_KEY, $_SESSION["access_token_data"] , $_SESSION["access_token_secret_data"]);
       $content = $connection->get('account/verify_credentials');
       $connection->post('statuses/update', array('status' => $_POST['status']));
     }
